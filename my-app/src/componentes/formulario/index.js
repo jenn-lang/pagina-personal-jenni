cecb5d329e2ee98bb44c9356aef930238eeb7ac1
jenn-lang <jennifervallejos08@gmail.com>
2020-02-07 17:13:37 -0300

    intente hacer el formulario en js

--- my-app/src/componentes/formulario/index.js
+++ my-app/src/componentes/formulario/index.js
@@ -1,13 +1,32 @@
-//import React from 'react';
-//import formulario from './componentes/formulario';
-
-
-//clases form extends.
+import React from 'react';
 
 
 
+class Form extends React.Component{
+    render(){
+        
+        return(
+            <div>
+                <h1>Formulario</h1>
+                <form>
+                <input
+                 type="texto" 
+                 id="username"
+                  name="username" 
+                  placeholder="nombre" 
+                  value={username}
+                  ></input>                    
+                <input id="username" name="username" placeholder="apellido" value></input>
+                </form>   
+            </div>
+        )
+    }
+}
 
 
 
 
-//export default formulario;//
+
+
+
+export default Form;
